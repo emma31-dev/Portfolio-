e44e1e2e3787e6171ec15e0694c1dc8ec811a5e2
emma31-dev <emmanuelfidel07@gmail.com>
2026-05-30 12:58:58 +0100

    updated portfolio to have minimalistic about content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from './components/Hero';
+import About from './components/About';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <About />
     </div>
   )
 }
