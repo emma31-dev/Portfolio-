537d7b4f5431405ed71492ee4d2be1363b5bf5b3
emma31-dev <emmanuelfidel07@gmail.com>
2026-07-02 15:14:06 +0100

    fixed local icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import '@fontsource/inter/400.css';
+import '@fontsource/jost/300.css';
+import '@fontsource/jost/400.css';
+import '@fontsource/jost/500.css';
+import '@fontsource/jost/600.css';
+import '@fontsource/jost/700.css';
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
